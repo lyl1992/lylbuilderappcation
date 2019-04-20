5f735c137594b19f6c0adc84f0c12bc0252d5b7a
liuyalin <331325767@qq.com>
2019-04-20 15:07:36 +0800

    第一次提交代码到github上

---
+++ app/src/main/java/com/myself/lyl/lylbuilderapplication/LYLMainActivity.java
@@ -0,0 +1,13 @@
+package com.myself.lyl.lylbuilderapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LYLMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lylmain);
+    }
+}
